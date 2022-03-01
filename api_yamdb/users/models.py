25bd861f0c9779fdf5d19a97d5afb0570c6b67c1
Серегей Токарев <sat0304@yandex.ru>
2022-03-01 18:00:04 +0400

    auth field was deleted to user

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,8 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class User(AbstractUser):
-    auth = models.TextField(
-        'Аутентификация',
-    )
+# Create your models here.
