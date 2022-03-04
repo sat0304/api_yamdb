76c1ab63353b86f9b27971e723a544986a1bc54a
Серегей Токарев <sat0304@yandex.ru>
2022-03-04 23:03:49 +0400

    serializer was  added

---
+++ api_yamdb/reviews/serializers.py
@@ -0,0 +1,42 @@
+# from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from .models import Comment, Review
+# from api.models import Category, Genre, Title
+# from users.models import User
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+        model = Review
+        
+    def validate(self, data):
+        super().validate(data)
+        if self.context['request'].method != 'POST':
+            return data
+        user = self.context['request'].user
+        title_id = (
+            self.context['request'].parser_context['kwargs']['title_id']
+        )
+        if Review.objects.filter(author=user, title__id=title_id).exists():
+            raise serializers.ValidationError(
+                "Можно оставить лишь один отзыв на данное произведение"
+            )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = ('id', 'text', 'author', 'pub_date',)
+        model = Comment
--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.db import IntegrityError
+from django.db.models import Avg
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, status, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly
+)
 
-# Create your views here.
+from .models import  Review, Comment
+from api.models import  Title
+from .permissions import (
+    IsAdminOrReadOnly,
+    IsAdminOrSuperUser,
+    ReviewCommentPermissions
+)
+from .serializers import CommentSerializer, ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [ReviewCommentPermissions, IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title'))
+        return Review.objects.filter(title=title)
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title'))
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [ReviewCommentPermissions, IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return Comment.objects.filter(review=review)
