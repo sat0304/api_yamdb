a1f695423ada485c6b84b4f3bbe0c220ed7c42d4
Сергей Токарeв <sat0304@yandex.ru>
2022-03-09 17:52:04 +0400

    settings добавлены токены

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitlesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Title
+        fields = ('__all__')
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
+from api.views import CategoryViewSet, GenreViewSet, TitlesViewSet
 
 app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
 
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('titles', TitlesViewSet, basename='titles')
+
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/', include('users.urls', namespace='users')),
+    path('v1/users/', include('users.urls', namespace='users')),
+    path('v1/auth/', include('users.urls', namespace='users')),
     path(
         'v1/titles/<int:title_id>/',
         include(
